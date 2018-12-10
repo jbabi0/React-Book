f260cc3108923a5c77876c4c0ab74c1681805c2e
Jeff Babio <babiojeff@gmail.com>
2018-12-10 13:09:21 -0500

    React Book

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+let books = [
+  {
+    'title': 'The Grinch',
+    'id': 101,
+    'author': 'Dr. Seuss',
+    'url':'https://www.imdb.com/title/tt0170016/'
+  },
+  {
+    'title': 'Red Fish, Blue Fish',
+    'id': 102,
+    'author': 'Dr. Seuss',
+    'url':'https://www.amazon.com/Fish-Blue-Myself-Beginner-Books/dp/0756921333'
+  }
+];
+
+
 class App extends Component {
   render() {
+    // Thhis javascript
+    const s = 'Hello, World';
+
+    // below this line, within the return is JSX
     return (
       <div className="App">
+      <h2>{this.props.name}</h2>
+      {/* This is JSX inside of the retrun parenthesis */}
+      {books.map(book =>
+        <div key={book.id}>
+          <span>
+          <a href={book.url} target="_blank">{book.title}</a>
+          </span>
+          <span> {book.author} </span>
+        </div>
+      )}
 
       </div>
-    );
+    ); // this is javascript again
   }
 }
 
